ec3a9c3e5306e908f1527f347803b5f82a073af2
Nico Aricayos <nicolenevanz.aricayos@gmail.com>
2023-09-04 23:59:05 +0800

    added unfinished HEADER

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,28 @@
-"use client"
+"use client";
 
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
-import { ModeToggle } from './components/mode_toggle'
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import { ModeToggle } from "./components/mode_toggle";
+import { UserNav } from "./components/user-nav";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-      <ModeToggle/>
-      
-      
-      <Button>Button</Button>
-      
-      
+    <main className="flex min-h-screen flex-col items-left justify-between">
+      <div className="border-b">
+        <div className="flex h-16 items-left px-4">
+          <div className="ml-auto flex items-center space-x-4 flex-grow justify-end"> {/* Updated */}
+            <ModeToggle />
+            <UserNav />
+            {/* <div>
+              <Input
+                type="search"
+                placeholder="Search..."
+                className="md:w-[100px] lg:w-[300px]"
+              />
+            </div> */}
+          </div>
+        </div>
+      </div>
     </main>
-  )
+  );
 }
