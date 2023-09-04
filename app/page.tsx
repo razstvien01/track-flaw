220d5ff21a29c7379272ba896b3cb5382a106652
Nico Aricayos <nicolenevanz.aricayos@gmail.com>
2023-09-04 23:42:03 +0800

    removed change themes

--- app/page.tsx
+++ app/page.tsx
@@ -3,23 +3,17 @@
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import { ModeToggle } from './components/mode_toggle'
-import { ThemeWrapper } from '@/components/theme-wrapper'
-import { ThemeCustomizer } from '@/components/theme_customizer'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       
       <ModeToggle/>
-      <ThemeCustomizer/>
-      
-      <ThemeWrapper>
       
       
       <Button>Button</Button>
       
       
-      </ThemeWrapper>
     </main>
   )
 }
