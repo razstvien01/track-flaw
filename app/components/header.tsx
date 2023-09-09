5074b3f5ac9e1c68a40f9d545a03b0945cbfa258
Nico Aricayos <nicolenevanz.aricayos@gmail.com>
2023-09-09 19:58:10 +0800

    updated header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -14,7 +14,7 @@ const Header = () => {
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
         <Logo />
-        <MainNav className="mx-6" />
+        <MainNav className="mx-6"  />
         <div className="ml-auto flex items-center space-x-4">
           <Search />
           <OrgSwitcher />
