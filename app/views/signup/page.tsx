779321cb92cc2da49cda408d4d775b5d1128ccc7
Nico Aricayos <nicolenevanz.aricayos@gmail.com>
2023-09-16 13:32:25 +0800

    added bg picture

--- app/views/signup/page.tsx
+++ app/views/signup/page.tsx
@@ -9,6 +9,7 @@ import { Button, buttonVariants } from "@/components/ui/button";
 import { UserAuthSignupForm } from "./components/user_auth_form_sign_up";
 import { useState } from "react";
 import ModeToggle from "@/app/components/mode-toggle";
+import { Logo } from "@/app/components/logo";
 
 export const metadata: Metadata = {
   title: "Authentication",
@@ -25,24 +26,8 @@ const AuthenticationPage: React.FC<any> = () => {
 
   return (
     <>
-      <div className="md:hidden">
-        <Image
-          src="/examples/authentication-light.png"
-          width={1280}
-          height={843}
-          alt="Authentication"
-          className="block dark:hidden"
-        />
-        <Image
-          src="/examples/authentication-dark.png"
-          width={1280}
-          height={843}
-          alt="Authentication"
-          className="hidden dark:block"
-        />
-      </div>
       <div className="container relative min-h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        <div className="flex items-center space-x-4 absolute right-4 top-4 md:right-8 md:top-8">
+        <div className="flex items-center space-x-4 absolute left-4 top-4 md:left-8 md:top-8">
           <ModeToggle /> {/* Place the ModeToggle component here */}
           <Button
             className={cn(
@@ -54,34 +39,7 @@ const AuthenticationPage: React.FC<any> = () => {
             {isSignUp ? "Login" : "Sign Up"}
           </Button>
         </div>
-        <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-          <div className="absolute inset-0 bg-zinc-900" />
-          <div className="relative z-20 flex items-center text-lg font-medium">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="mr-2 h-6 w-6"
-            >
-              <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-            </svg>
-            Acme Inc
-          </div>
-          <div className="relative z-20 mt-auto">
-            <blockquote className="space-y-2">
-              <p className="text-lg">
-                &ldquo;This library has saved me countless hours of work and
-                helped me deliver stunning designs to my clients faster than
-                ever before.&rdquo;
-              </p>
-              <footer className="text-sm">Sofia Davis</footer>
-            </blockquote>
-          </div>
-        </div>
+
         <div className="lg:p-8">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
@@ -110,6 +68,23 @@ const AuthenticationPage: React.FC<any> = () => {
               </Link>
               .
             </p>
+          </div>
+        </div>
+        <div className="relative h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex bg-cover bg-center" style={{ backgroundImage: 'url("/peakpx.jpg")' }}>
+          <div className="absolute inset-0" />
+          <div className="relative">
+            <div className="absolute top-1 right-4 flex items-center text-lg font-medium md:right-8 md:bottom-8">
+              <Logo/>
+              &nbsp;Buggy Cat
+            </div>
+          </div>
+          <div className="relative z-20 mt-auto">
+            <blockquote className="space-y-2">
+              <p className="text-lg">
+                &ldquo;{"The Track Flaw's powerful features have saved me countless hours of work and allowed me to deliver flawless software to my clients faster than ever before."}&rdquo;
+              </p>
+              <footer className="text-sm">Mr. Robot</footer>
+            </blockquote>
           </div>
         </div>
       </div>
