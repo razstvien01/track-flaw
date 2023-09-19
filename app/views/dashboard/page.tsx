f2a8375a317cc3bb76b13dc42feeb2faf1689fb0
Nico Aricayos <nicolenevanz.aricayos@gmail.com>
2023-09-19 19:00:26 +0800

    checkpoint

--- app/views/dashboard/page.tsx
+++ app/views/dashboard/page.tsx
@@ -7,12 +7,12 @@ import React, { useEffect } from "react";
 const Dashboard = () => {
   const [userData, setUserData] = useUserDataAtom();
   const { org_refs = []} = userData || {}
-  // TODO make a selected Organization hook atom
+  // TODO make a selected Organization hook atom, make an additional attribute for the organization which is the details
   console.log(userData);
   return (
     <div>
       <PageHeader>
-        <PageHeaderHeading>Welcome to the Dashboard!!!</PageHeaderHeading>
+        <PageHeaderHeading>Welcome to the Dashboard !!!</PageHeaderHeading>
         <PageHeaderDescription>Please select an organization.</PageHeaderDescription>
       </PageHeader>
     </div>
