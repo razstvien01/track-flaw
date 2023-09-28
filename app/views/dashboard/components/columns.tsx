494164349cdd909954a6035c59c30b759033ced5
LAB23_USER ON WS25 <L23Y16W25@citulab.net>
2023-09-28 17:54:55 +0800

    added name, id, picture and phone number in the org table

--- app/views/dashboard/components/columns.tsx
+++ app/views/dashboard/components/columns.tsx
@@ -60,40 +60,17 @@ export const columns: ColumnDef<Task>[] = [
       <DataTableColumnHeader column={column} title="ID" />
     ),
     cell: ({ row }) => {
-      return <div className="w-[80px]">{row.getValue("id")}</div>;
-      //   const label = labels.find((label) => label.value === row.original.name);
-
-      //   return (
-      //     <div className="flex space-x-2">
-      //       {label && <Badge variant="outline">{label.label}</Badge>}
-      //       <span className="max-w-[500px] truncate font-medium">
-      //         {row.getValue("name")}
-      //       </span>
-      //     </div>
-      //   );
+      return <div className="w-[80px] ">{row.getValue("id")}</div>;
     },
   },
   {
-    accessorKey: "status",
+    accessorKey: "name",
     header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Status" />
+      <DataTableColumnHeader column={column} title="Name" />
     ),
     cell: ({ row }) => {
-      const status = statuses.find(
-        (status) => status.value === row.getValue("status")
-      );
-
-      if (!status) {
-        return null;
-      }
-
       return (
-        <div className="flex w-[100px] items-center">
-          {status.icon && (
-            <status.icon className="mr-2 h-4 w-4 text-muted-foreground" />
-          )}
-          <span>{status.label}</span>
-        </div>
+        <div className="w-[150px]">{row.getValue("name")}</div>
       );
     },
     filterFn: (row, id, value) => {
@@ -101,27 +78,12 @@ export const columns: ColumnDef<Task>[] = [
     },
   },
   {
-    accessorKey: "priority",
+    accessorKey: "phone_number",
     header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Priority" />
+      <DataTableColumnHeader column={column} title="Phone Number" />
     ),
     cell: ({ row }) => {
-      const priority = priorities.find(
-        (priority) => priority.value === row.getValue("priority")
-      );
-
-      if (!priority) {
-        return null;
-      }
-
-      return (
-        <div className="flex items-center">
-          {priority.icon && (
-            <priority.icon className="mr-2 h-4 w-4 text-muted-foreground" />
-          )}
-          <span>{priority.label}</span>
-        </div>
-      );
+      return <div className="w-[80px] ">{row.getValue("phone_number")}</div>;
     },
     filterFn: (row, id, value) => {
       return value.includes(row.getValue(id));
