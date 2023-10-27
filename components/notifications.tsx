f35f174485cd723e7c06be52f0ededf7d8327d69
Nico Aricayos <nicolenevanz.aricayos@gmail.com>
2023-10-27 10:07:12 +0800

    update notifications

--- components/notifications.tsx
+++ components/notifications.tsx
@@ -5,44 +5,53 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
+  DropdownMenuGroup,
 } from "@/components/ui/dropdown-menu";
 import { ScrollArea } from "@/components/ui/scroll-area";
 
-import { Separator } from "@/components/ui/separator"
+import { Separator } from "@/components/ui/separator";
 
 import { BellIcon } from "@radix-ui/react-icons";
 import { useTheme } from "next-themes";
+import { Button } from "./ui/button";
 
 const tags = Array.from({ length: 50 }).map(
   (_, i, a) => `v1.2.0-beta.${a.length - i}`
-)
+);
 
 const Notifications = () => {
   const { theme, setTheme } = useTheme();
-  
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
-        {theme === 'light' ? (
+        {theme === "light" ? (
           <BellIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
         ) : (
           <BellIcon className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
         )}
-        
       </DropdownMenuTrigger>
       <DropdownMenuContent>
         <ScrollArea className="h-72 w-48 rounded-md">
-          <div className="p-4">
-            <h4 className="mb-4 text-sm font-medium leading-none">Tags</h4>
+          <DropdownMenuGroup className="p-4">
+            <DropdownMenuLabel className="mb-4 text-lg leading-none font-bold">
+              Notifications
+            </DropdownMenuLabel>
             {tags.map((tag) => (
               <>
-                <div key={tag} className="text-sm">
+                <DropdownMenuItem
+                  key={tag}
+                  className="text-sm"
+                  onClick={() => {
+                    console.log("Pressed");
+                  }}
+                >
                   {tag}
-                </div>
+                </DropdownMenuItem>
                 <Separator className="my-2" />
               </>
             ))}
-          </div>
+          </DropdownMenuGroup>
         </ScrollArea>
       </DropdownMenuContent>
     </DropdownMenu>
